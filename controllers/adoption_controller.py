5e65bc7984bc943b97c2a5eb48259e60c4c470e8
Daniel <dsalvatori2008@gmail.com>
2022-12-20 11:47:41 +0000

    all setup done

--- controllers/adoption_controller.py
+++ controllers/adoption_controller.py
@@ -1,1 +1,7 @@
-_
+from flask import render_template, Blueprint, redirect, request
+
+from repositories import animal_repository
+from repositories import shelter_repository
+
+adoption_blueprint = Blueprint("animals", __name__)
+
